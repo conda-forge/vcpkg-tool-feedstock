1d9c7c3974696b2f37cc6c032c2ae9fbb30005eb
AntoinePrv <AntoinePrv@users.noreply.github.com>
2023-06-07 18:02:54 +0200

    TMP debug info

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,17 @@
 setlocal EnableDelayedExpansion
 setlocal enableextensions
 
+echo "--------------------"
+echo %%CXX%%
+echo %%CC%%
+
 REM As long as we use a custom msvc in conda_build_config.yaml
 for /f "delims=" %%i in ('where "%CC%"') do set "CC=%%i"
 if errorlevel 1 exit 1
 for /f "delims=" %%i in ('where "%CXX%"') do set "CXX=%%i"
 if errorlevel 1 exit 1
+echo %%CXX%%
+echo %%CC%%
 
 cmake ^
 	-B build ^
